dffd7fc4bdabfe0264d8132a503573cfa5a67eb0
nila akter <akternila1147@gmail.com>
2024-10-07 22:01:31 +0600

    pets

--- js/fetch.js
+++ js/fetch.js
@@ -12,7 +12,7 @@ const displayCategory = (data) => {
     const buttonContainer =document.createElement("div");
    buttonContainer.innerHTML=
    `
-   <button class="btn btn-lg px-16" id="${item.category}" onclick="loadPetsByCategory (${item.category})">
+   <button onclick="loadPetsByCategory ('${item.category}')" class="btn btn-lg px-16" id="${item.category}" >
  <img class="w-8  " src="${item.category_icon}">
  ${item.category}
    </button>
@@ -24,13 +24,18 @@ const displayCategory = (data) => {
 
 // load pets by category
 const loadPetsByCategory = (id)=>{
-  
+ alert(id)
+  fetch(`https://openapi.programming-hero.com/api/peddy/${id}`)
+  .then((res) => res.json())
+  .then((data) =>console.log(data.data))
+  .catch((error) => console.log('errored'))
    
 };
 
 
 // all pets
 const loadPets= () =>{
+ 
     fetch(`https://openapi.programming-hero.com/api/peddy/pets`)
     .then((res) => res.json())
     .then((data) =>displayLoadPets(data.pets))
@@ -38,16 +43,17 @@ const loadPets= () =>{
 }
 const displayLoadPets = (pet) =>{
    const petsContainer = document.getElementById('all-pets');
+   petsContainer.innerHTML="";
    pet.forEach((item) => {
     const div = document.createElement('div');
-    div.classList="flex"
+    div.classList=""
     div.innerHTML=`
-   <div class="grid grid-cols-4">
+   <div class="">
    <div class="card card-compact ">
   <figure class="w-[300px] h-[300px]">
     <img class="w-full h-full object-cover" src=${item.image} />
   </figure>
-  <div>
+  <div class="mt-3">
     <div class="flex gap-2 items-center">
     <i class="fa-solid fa-border-all"></i>
     <p>Breed: ${item.breed?.length==0? "Undefined" : `${item.breed}`}</p>
@@ -60,7 +66,7 @@ const displayLoadPets = (pet) =>{
 
      <div class="flex gap-2">
    <i class="fa-solid fa-mercury"></i>
-     <p>Birth: ${item.date_of_birth?.length==0? "Undefined" : `${item.date_of_birth}`}</p>
+     <p>Birth: ${item.gender?.length==0? "Undefined" : `${item.gender}`}</p>
     </div>
 
      <div class="flex gap-2">
@@ -69,24 +75,36 @@ const displayLoadPets = (pet) =>{
     </div>
 
    </div>
-   <div>
-   <button class="btn"><i class="fa-solid fa-thumbs-up"></i></button>
-   <button class="btn">Adopt</button>
-   <button class="btn">Details</button>
+   <div class="my-4  ">
+   <button class="btn text-[#0E7A81]"><i class="fa-solid fa-thumbs-up"></i></button>
+   <button class="btn text-[#0E7A81]">Adopt</button>
+   <button onclick=loadPetDetails("${item.petId}") class="btn text-[#0E7A81]">Details</button>
    </div>
   
 
 </div>
-   </div>
-
-   <div class="w-1/4 bg-black">
-   </div>
+  
 
     `
     petsContainer.append(div)
    })
 };
+// pet details by petId
+const loadPetDetails = async(pet) => {
+  console.log(pet)
+  const uri=`https://openapi.programming-hero.com/api/peddy/pet/${pet}`;
+  const res = await fetch(uri);
+  const data = await res.json();
+  displayPetDetails(data.petData);
+}
+const displayPetDetails = (detail) =>{
+console.log(detail)
+const detailContainer = getElementById('modal-content')
+
+// document.getElementById("showModalData").click();
+document.getElementById("customModal").showModal();
+}
 
 loadCategories();
 loadPets();
-loadPetsByCategory();
+
